fdef0d0ac2c96c22e03a166b947699bbd7bf42dc
Kaisar Arkhan <ykno@protonmail.com>
2016-11-02 20:56:51 +0700

    Fix bug on unit test for generators

--- test/generators_test.rb
+++ test/generators_test.rb
@@ -11,7 +11,7 @@ class GeneratorsTest < Minitest::Test
     assert Ayanami::Generator.force_reply.is_a? Hash
     assert Ayanami::Generator.input_text_message_content.is_a? Hash
     assert Ayanami::Generator.input_location_message_content.is_a? Hash
-    assert Ayanami::Generator.input_venue_message.content.is_a? Hash
+    assert Ayanami::Generator.input_venue_message_content.is_a? Hash
     assert Ayanami::Generator.input_contact_message_content.is_a? Hash
     assert Ayanami::Generator.inline_keyboard_button.is_a? Hash
   end
